1e8d6af53bf1163d0a0c502073c0c844a987033e
cynthiaoduol <cynthiaobu940@gmail.com>
2022-10-19 11:30:04 +0300

    link bootstrap

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to PlusTree</h1>
+      <button className='btn-warning'>click me</button>
     </div>
   )
 }
